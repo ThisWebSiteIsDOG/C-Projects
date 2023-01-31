bb10a3e6274803acadb1334d98ea8307ce89e63d
ThisWebSiteIsDOG <123392631+ThisWebSiteIsDOG@users.noreply.github.com>
2023-01-31 16:00:57 -0600

    Add files via upload

---
+++ Week 2 Assignment/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Week_2_Assignment_2_Assignment
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 7;
+            
+
+
+            myVar =  - 1 + 4 * myVar;
+
+            Console.WriteLine(myVar);
+
+            myVar = (35 + 5);
+
+            myVar %=  7;
+
+            Console.WriteLine(myVar);
+
+            myVar = 14 + -4 * 6 / 12;
+
+            Console.WriteLine(myVar);
+
+            myVar = 2 + 12 / 6 * 1 - myVar % 2;
+           
+
+            Console.WriteLine(myVar);
+
+
+            if (myVar * myVar < 10) 
+            {
+                Console.WriteLine("myVar is less than 4");
+            }
+            else
+            {
+                Console.WriteLine("myVar is greater than 4");
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
